94fe37e81e6473f49aee4941d2900a9199320cac
Anders Urrang <anders.urrang@unimicro.no>
2023-12-01 21:07:07 +0100

    day1 update

--- typescript/src/day1/index.ts
+++ typescript/src/day1/index.ts
@@ -18,15 +18,23 @@ function part2() {
     const wordToDigit = { 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9 };
 
     for (let line of content.split('\n')) {
-        const digits = [...line.matchAll(/\d/g)].map(match => ({ index: match.index, value: Number(match[0]) }));
+        const digits = [];
 
-        for (let str in wordToDigit) {
-            const matches = [...line.matchAll(str as any)];
-            matches.forEach(match => digits.push({ index: match.index, value: Number(wordToDigit[str]) }));
-        }
+        while (line.length) {
+            if (Number(line[0])) {
+                digits.push(Number(line[0]));
+            } else {
+                for (const word in wordToDigit) {
+                    if (line.startsWith(word)) {
+                        digits.push(wordToDigit[word]);
+                    }
+                }
+            }
 
-        digits.sort((a, b) => a.index - b.index);
-        sum += Number(digits.at(0).value + '' + digits.at(-1).value);
+            line = line.slice(1);
+        };
+
+        sum += Number(digits.at(0) + '' + digits.at(-1));
     }
 
     console.log(sum);
