95085c83a6c0a132533aba10142091f3df488690
Anders Urrang <anders.urrang@unimicro.no>
2023-12-05 20:53:38 +0100

    optimize day5 part2

--- typescript/src/solutions/day5.ts
+++ typescript/src/solutions/day5.ts
@@ -1,43 +1,6 @@
 import { readFile } from '../utils';
 
-const example = `seeds: 79 14 55 13
-
-seed-to-soil map:
-50 98 2
-52 50 48
-
-soil-to-fertilizer map:
-0 15 37
-37 52 2
-39 0 15
-
-fertilizer-to-water map:
-49 53 8
-0 11 42
-42 0 7
-57 7 4
-
-water-to-light map:
-88 18 7
-18 25 70
-
-light-to-temperature map:
-45 77 23
-81 45 19
-68 64 13
-
-temperature-to-humidity map:
-0 69 1
-1 0 69
-
-humidity-to-location map:
-60 56 37
-56 93 4
-`;
-
-const input = readFile('src/inputs/day5.txt');
-
-const groups = input.split('\n\n');
+const groups = readFile('src/inputs/day5.txt').split('\n\n');
 
 const seeds = groups
     .shift()
@@ -45,19 +8,17 @@ const seeds = groups
     .split(' ')
     .map(x => parseInt(x));
 
-
 const maps = groups.map((group) => {
-    const lines = group.split('\n');
-    lines.shift();
+    const lines = group.split('\n').slice(1);
     return lines.map(line => {
         const values = line.split(' ');
-
         return {
             destStart: parseInt(values[0]),
+            destEnd: parseInt(values[0]) + parseInt(values[2]),
             sourceStart: parseInt(values[1]),
             sourceEnd: parseInt(values[1]) + parseInt(values[2]),
-        };
-    }).sort((a, b) => a.sourceStart - b.sourceStart);
+        }
+    });
 })
 
 function getDestination(source: number, map: any[]) {
@@ -87,27 +48,43 @@ function part1() {
     console.log(minLocation);
 }
 
-function part2() {
-    let min = Infinity;
-
+function isSeed(value: number) {
     for (let i = 0; i < seeds.length; i += 2) {
-        const first = seeds[i];
-        const last = seeds[i] + seeds[i + 1] - 1;
-
-        for (let j = first; j <= last; j++) {
-            let value = j;
-
-            for (const map of maps) {
-                value = getDestination(value, map);
-            }
-
-            if (value < min) {
-                min = value;
-            }
+        if (value >= seeds[i] && value < seeds[i] + seeds[i + 1]) {
+            return true;
         }
     }
 
-    console.log(min);
+    return false;
+}
+
+function getSource(dest: number, map: any[]) {
+    for (const item of map) {
+        if (dest >= item.destStart && dest < item.destEnd) {
+            return item.sourceStart + (dest - item.destStart);
+        }
+    }
+
+    return dest;
+}
+
+function part2() {
+    const mapsReversed = maps.toReversed();
+
+    let location = 0;
+    while ('yolo') {
+        let value = location;
+        for (const map of mapsReversed) {
+            value = getSource(value, map);
+        }
+
+        if (isSeed(value)) {
+            console.log(location);
+            break;
+        }
+
+        location++;
+    }
 }
 
 export default function day5() {
